f90f950c911e1efcc28fb42c5cdfc10bde3ceeea
Nicholas Martin <nickmartin1ee7@gmail.com>
2022-10-13 18:57:12 -0400

    More renames

--- Prog2/Prog2Form.Designer.cs
+++ Prog2/Prog2Form.Designer.cs
@@ -31,14 +31,14 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Prog2Form));
             this.toolStripMain = new System.Windows.Forms.ToolStrip();
             this.toolStripDropDownButtonFile = new System.Windows.Forms.ToolStripDropDownButton();
-            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemExit = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripDropDownButtonInsert = new System.Windows.Forms.ToolStripDropDownButton();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
-            this.toolStripDropDownButtonReport = new System.Windows.Forms.ToolStripDropDownButton();
             this.toolStripMenuItemAddress = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripMenuItemLetter = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButtonReport = new System.Windows.Forms.ToolStripDropDownButton();
+            this.toolStripMenuItemListAddress = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemListParcels = new System.Windows.Forms.ToolStripMenuItem();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.toolStripMain.SuspendLayout();
             this.SuspendLayout();
@@ -58,73 +58,73 @@
             // 
             this.toolStripDropDownButtonFile.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.toolStripDropDownButtonFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem3,
-            this.toolStripMenuItem4});
+            this.toolStripMenuItemAbout,
+            this.toolStripMenuItemExit});
             this.toolStripDropDownButtonFile.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButtonFile.Image")));
             this.toolStripDropDownButtonFile.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripDropDownButtonFile.Name = "toolStripDropDownButtonFile";
             this.toolStripDropDownButtonFile.Size = new System.Drawing.Size(38, 22);
             this.toolStripDropDownButtonFile.Text = "File";
             // 
-            // toolStripMenuItem3
+            // toolStripMenuItemAbout
             // 
-            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
-            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItem3.Text = "About";
+            this.toolStripMenuItemAbout.Name = "toolStripMenuItemAbout";
+            this.toolStripMenuItemAbout.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemAbout.Text = "About";
             // 
-            // toolStripMenuItem4
+            // toolStripMenuItemExit
             // 
-            this.toolStripMenuItem4.Name = "toolStripMenuItem4";
-            this.toolStripMenuItem4.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItem4.Text = "Exit";
+            this.toolStripMenuItemExit.Name = "toolStripMenuItemExit";
+            this.toolStripMenuItemExit.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemExit.Text = "Exit";
             // 
             // toolStripDropDownButtonInsert
             // 
             this.toolStripDropDownButtonInsert.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.toolStripDropDownButtonInsert.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItem1,
-            this.toolStripMenuItem2});
+            this.toolStripMenuItemAddress,
+            this.toolStripMenuItemLetter});
             this.toolStripDropDownButtonInsert.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButtonInsert.Image")));
             this.toolStripDropDownButtonInsert.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripDropDownButtonInsert.Name = "toolStripDropDownButtonInsert";
             this.toolStripDropDownButtonInsert.Size = new System.Drawing.Size(49, 22);
             this.toolStripDropDownButtonInsert.Text = "Insert";
             // 
-            // toolStripMenuItem1
+            // toolStripMenuItemAddress
             // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItem1.Text = "Address";
+            this.toolStripMenuItemAddress.Name = "toolStripMenuItemAddress";
+            this.toolStripMenuItemAddress.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemAddress.Text = "Address";
             // 
-            // toolStripMenuItem2
+            // toolStripMenuItemLetter
             // 
-            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItem2.Text = "Letter";
+            this.toolStripMenuItemLetter.Name = "toolStripMenuItemLetter";
+            this.toolStripMenuItemLetter.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemLetter.Text = "Letter";
             // 
             // toolStripDropDownButtonReport
             // 
             this.toolStripDropDownButtonReport.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
             this.toolStripDropDownButtonReport.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripMenuItemAddress,
-            this.toolStripMenuItemLetter});
+            this.toolStripMenuItemListAddress,
+            this.toolStripMenuItemListParcels});
             this.toolStripDropDownButtonReport.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButtonReport.Image")));
             this.toolStripDropDownButtonReport.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripDropDownButtonReport.Name = "toolStripDropDownButtonReport";
             this.toolStripDropDownButtonReport.Size = new System.Drawing.Size(55, 22);
             this.toolStripDropDownButtonReport.Text = "Report";
             // 
-            // toolStripMenuItemAddress
+            // toolStripMenuItemListAddress
             // 
-            this.toolStripMenuItemAddress.Name = "toolStripMenuItemAddress";
-            this.toolStripMenuItemAddress.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItemAddress.Text = "List Addresses";
+            this.toolStripMenuItemListAddress.Name = "toolStripMenuItemListAddress";
+            this.toolStripMenuItemListAddress.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemListAddress.Text = "List Addresses";
             // 
-            // toolStripMenuItemLetter
+            // toolStripMenuItemListParcels
             // 
-            this.toolStripMenuItemLetter.Name = "toolStripMenuItemLetter";
-            this.toolStripMenuItemLetter.Size = new System.Drawing.Size(180, 22);
-            this.toolStripMenuItemLetter.Text = "List Parcels";
+            this.toolStripMenuItemListParcels.Name = "toolStripMenuItemListParcels";
+            this.toolStripMenuItemListParcels.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItemListParcels.Text = "List Parcels";
             // 
             // textBox1
             // 
@@ -157,14 +157,14 @@
 
         private ToolStrip toolStripMain;
         private ToolStripDropDownButton toolStripDropDownButtonReport;
+        private ToolStripMenuItem toolStripMenuItemListAddress;
+        private ToolStripMenuItem toolStripMenuItemListParcels;
+        private ToolStripDropDownButton toolStripDropDownButtonFile;
+        private ToolStripMenuItem toolStripMenuItemAbout;
+        private ToolStripMenuItem toolStripMenuItemExit;
+        private ToolStripDropDownButton toolStripDropDownButtonInsert;
         private ToolStripMenuItem toolStripMenuItemAddress;
         private ToolStripMenuItem toolStripMenuItemLetter;
-        private ToolStripDropDownButton toolStripDropDownButtonFile;
-        private ToolStripMenuItem toolStripMenuItem3;
-        private ToolStripMenuItem toolStripMenuItem4;
-        private ToolStripDropDownButton toolStripDropDownButtonInsert;
-        private ToolStripMenuItem toolStripMenuItem1;
-        private ToolStripMenuItem toolStripMenuItem2;
         private TextBox textBox1;
     }
 }
